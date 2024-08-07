47409513547b861245e38bc1fe85d5e0b1ff12a4
unknown <arnaud.defrancq@laposte.net>
2024-08-07 15:45:11 +0200

    [+] POSTLIKE : Ajout du model

---
+++ API/src/models/PostLikeModel.ts
@@ -0,0 +1,30 @@
+import { Dao } from "../DAO/DAO";
+import PostLike from "../types/PostLike";
+
+
+export class PostModel {
+    private postLikeDao: Dao<PostLike>;
+
+    constructor() {
+        this.postLikeDao = new Dao('post_like');
+    }
+    
+    public createPostLike(postLike: PostLike, callback: (error: Error | null, insertId?: number) => void) : void {
+        return this.postLikeDao.create(postLike, callback);
+      }
+    
+      public findPostLike(where: string, select: string = "*"): Promise<PostLike[]> {
+        return  this.postLikeDao.find(where, select);
+      }
+
+      public findById(id: number, select: string = "*"): Promise<PostLike[]> {
+        return this.postLikeDao.findById(id, select);
+      }
+    
+      public updatePostLike(id: number, postLike: Partial<PostLike>, callback: (error: Error | null, affectedRows?: number) => void): void {
+        return this.postLikeDao.update(id, postLike, callback)
+      }
+      public deletePostLike(id: number,  callback: (error: Error | null, affectedRows?: number) => void) {
+        return this.postLikeDao.delete(id, callback);
+      }
+}
