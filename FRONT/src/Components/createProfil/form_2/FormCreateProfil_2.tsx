f88d0ade2303dc2ce368ed2d59be656d5ad5cc6c
unknown <arnaud.defrancq@laposte.net>
2024-09-10 19:42:09 +0200

    [+] CREATE PROFIL : data dans le localStorage profil + banner Ok

--- FRONT/src/Components/createProfil/form_2/FormCreateProfil_2.tsx
+++ FRONT/src/Components/createProfil/form_2/FormCreateProfil_2.tsx
@@ -1,13 +1,48 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Style from './FormCreateProfil_2.module.css';
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface IFormCreateProfil_2Props {
 }
 
 const FormCreateProfil_2: React.FunctionComponent<IFormCreateProfil_2Props> = () => {
-  const [imgProfil, setImgProfil] = useState<string>('../../../../public/images/profilDefault.png');
+  const [imgProfil, setImgProfil] = useState<string>('');
   const [imgBanner, setImgBanner] = useState<string>('../../../../public/images/bannerDefault.png');
+
+  // Permet de récup les données et de set les données dans les input
+  useEffect(() => {
+    if (localStorage.getItem('formData')) {
+      const storedData = localStorage.getItem('formData');
+      if (storedData) {
+        const parseData = JSON.parse(storedData);
+        if (parseData.profil)  {
+          setImgProfil(parseData.profil);      
+        } else {
+          setImgProfil('../../../../public/images/profilDefault.png')
+        }
+        if (parseData.banner)  {
+          setImgBanner(parseData.banner);      
+        } else {
+          setImgBanner('../../../../public/images/bannerDefault.png')
+        }
+      }
+    }
+  }, [])
+  
+
+  // Permet de set les données dans le localStorage
+  const setDataLocalStorage = (node:string, value: string):void => {
+    if (localStorage.getItem('formData')) {
+      const storedData = localStorage.getItem('formData');
+      if (storedData) {
+        const parseData = JSON.parse(storedData);
+        if (node in parseData) {          
+          parseData[node] = value
+        }
+        localStorage.setItem('formData', JSON.stringify(parseData))
+      }
+    }
+  }
 
   // Permet de visualiser la photo
   const previewImg = (e:React.ChangeEvent<HTMLInputElement>, profil: boolean = true):void => {
@@ -16,8 +51,10 @@ const FormCreateProfil_2: React.FunctionComponent<IFormCreateProfil_2Props> = ()
       reader.onload = (event: ProgressEvent<FileReader>) => {
         if (profil) {
           setImgProfil(event.target?.result as string);
+          setDataLocalStorage('profil', event.target?.result as string)
         } else {
           setImgBanner(event.target?.result as string);
+          setDataLocalStorage('banner', event.target?.result as string)
         }
       };
       reader.readAsDataURL(e.target.files[0]);
