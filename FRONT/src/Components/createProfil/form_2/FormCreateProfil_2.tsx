e8a74e366590478d5daa47419dd11f2a62dc680a
unknown <arnaud.defrancq@laposte.net>
2024-09-11 09:38:13 +0200

    [~] CREATE PROFIL : Changement verif input

--- FRONT/src/Components/createProfil/form_2/FormCreateProfil_2.tsx
+++ FRONT/src/Components/createProfil/form_2/FormCreateProfil_2.tsx
@@ -2,23 +2,24 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import Style from './FormCreateProfil_2.module.css';
 import { monthArray } from '../../../Tools/config';
+import { Security } from '../../../Tools/Security';
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface IFormCreateProfil_2Props {
+}
+
+type Errors = {
+  errorFirstName: boolean;
+  errorLastName: boolean;
+  errorDate: boolean;
+  errorCity: boolean;
+  errorContent: boolean;
+  errorProfil: boolean;
+  errorBanner: boolean;
 }
 
 const FormCreateProfil_2: React.FunctionComponent<IFormCreateProfil_2Props> = () => {
   const [imgProfil, setImgProfil] = useState<string>('');
   const [imgBanner, setImgBanner] = useState<string>('');
-
-  interface Errors {
-    errorFirstName: boolean;
-    errorLastName: boolean;
-    errorDate: boolean;
-    errorCity: boolean;
-    errorContent: boolean;
-    errorProfil: boolean;
-    errorBanner: boolean;
-  }
 
   const [errors, setErrors] = useState<Errors>({
     errorFirstName: true,
@@ -81,25 +82,6 @@ const FormCreateProfil_2: React.FunctionComponent<IFormCreateProfil_2Props> = ()
     }
   }
 
-  // Permet de check les inputs
-  const checkInput = (value: string, regex: RegExp): boolean  => {
-    return regex.test(value) && value != "";
-  }
-
-  const checkValidity = (value: string, regex: RegExp, node: string): void => {
-    if (checkInput(value, regex)) {
-        setErrors((elemnt) => ({
-            ...elemnt,
-            [node]: false
-        }));
-    } else {
-        setErrors((elemnt) => ({
-            ...elemnt,
-            [node]: true
-        }));
-    }
-  }
-
   const REGEX_DATE_NAISSANCE: RegExp = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
   const REGEX_TEXTE: RegExp = /^[a-zA-Z\s].{3,20}$/;
 
@@ -108,12 +90,12 @@ const FormCreateProfil_2: React.FunctionComponent<IFormCreateProfil_2Props> = ()
     const storedData = localStorage.getItem('formData');
     if (storedData) {
       const parseData = JSON.parse(storedData);
-      checkValidity(parseData.firstName, REGEX_TEXTE, 'errorFirstName')
-      checkValidity(parseData.lastName, REGEX_TEXTE, 'errorLastName')
-      checkValidity(parseData.city, REGEX_TEXTE, 'errorcity')
-      checkValidity(parseData.content, REGEX_TEXTE, 'errorContent')
-      checkValidity(parseData.profil, REGEX_TEXTE, 'errorProfil')
-      checkValidity(parseData.banner, REGEX_TEXTE, 'errorBanner')
+      Security.checkValidity(parseData.firstName, REGEX_TEXTE, 'errorFirstName', setErrors)
+      Security.checkValidity(parseData.lastName, REGEX_TEXTE, 'errorLastName', setErrors)
+      Security.checkValidity(parseData.city, REGEX_TEXTE, 'errorCity', setErrors)
+      Security.checkValidity(parseData.content, REGEX_TEXTE, 'errorContent', setErrors)
+      Security.checkValidity(parseData.profil, REGEX_TEXTE, 'errorProfil', setErrors)
+      Security.checkValidity(parseData.banner, REGEX_TEXTE, 'errorBanner', setErrors)
     }
   }
 
