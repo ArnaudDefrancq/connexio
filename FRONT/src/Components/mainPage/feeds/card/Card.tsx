4f40f5fd34d7b8af1d3104ce4739c688f1bfac60
unknown <arnaud.defrancq@laposte.net>
2024-10-02 08:31:37 +0200

    [+] POST : Get post OK

--- FRONT/src/Components/mainPage/feeds/card/Card.tsx
+++ FRONT/src/Components/mainPage/feeds/card/Card.tsx
@@ -15,7 +15,7 @@ const Card: React.FunctionComponent<ICardProps> = ({ post }) => {
         <Author nom={post.nom} prenom={post.prenom} img={post.img_profil} createdAt={post.created_at} updatedAt={post.updated_at} id_profil={post.id_profil} />
       </div>
       <div>
-        <Content content={post.content} media={post.media} />
+        <Content content={post.content} media={post.media} id_profil={post.id_profil}/>
       </div>
       <div>
         <Commentaire id_post={post.id_post}/>
--- FRONT/src/Components/mainPage/feeds/card/content/Content.tsx
+++ FRONT/src/Components/mainPage/feeds/card/content/Content.tsx
@@ -2,12 +2,25 @@ import * as React from 'react';
 
 interface IContentProps {
   content: string,
-  media: string | null | undefined
+  media: string | null | undefined,
+  id_profil: number | undefined
 }
 
-const Content: React.FunctionComponent<IContentProps> = () => {
+const Content: React.FunctionComponent<IContentProps> = ({ content, media, id_profil }) => {
+
+  let pathImg = '';
+  if (media) {
+    pathImg = `http://localhost:5000/imgPost/${id_profil}/${media}`;
+  }
+
   return (
     <>
+    <div>
+      <p>{content}</p>
+      {
+        (pathImg && <img src={pathImg} alt="photo poster" />)
+      }
+    </div>
     </>
   );
 };
