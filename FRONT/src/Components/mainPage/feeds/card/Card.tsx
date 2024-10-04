f09adad0893331225a8122736a0ff2c7e902b7cf
unknown <arnaud.defrancq@laposte.net>
2024-10-04 13:17:56 +0200

    [+] POST : Ajout des tools (supp + modif) que le style

--- FRONT/src/Components/mainPage/feeds/card/Card.tsx
+++ FRONT/src/Components/mainPage/feeds/card/Card.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Style from './Card.module.css';
 import { PostWithProfil } from '../../../../Types/Post';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisVertical, faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 import Author from './author/Author';
 import Content from './content/Content';
 import Commentaire from './commentaire/Commentaire';
@@ -17,13 +17,26 @@ const Card: React.FunctionComponent<ICardProps> = ({ post }) => {
 
   const { role, id_user } = useContext(UserContext);
 
+  const [isClick, setIsClick] = useState<boolean>(false);
+
+  const handleClick = ():void => {
+    setIsClick(prevState => !prevState);
+  }
+
   return (
     <>
       <div className={Style.card}>
         <div className={Style.cardAuthor}>
           <Author nom={post.nom} prenom={post.prenom} img={post.img_profil} createdAt={post.created_at} updatedAt={post.updated_at} id_profil={post.id_profil} />
           {
-            (role === 1 || id_user === post.id_profil) && <button className={Style.button}><FontAwesomeIcon className={Style.icon} icon={faEllipsisVertical}/></button>
+            (role === 1 || id_user === post.id_profil) && <button className={Style.button} onClick={handleClick}><FontAwesomeIcon className={Style.icon} icon={faEllipsisVertical}/></button>
+          }
+          {
+            (isClick) && 
+            <div className={Style.divTools}>
+              <button><FontAwesomeIcon className={Style.iconTools} icon={faPencil}/><span>Modifier</span></button>
+              <button><FontAwesomeIcon className={Style.iconTools} icon={faTrash}/><span>Supprimer</span></button>
+            </div> 
           }
         </div>
         <div className={Style.cardPost}>
