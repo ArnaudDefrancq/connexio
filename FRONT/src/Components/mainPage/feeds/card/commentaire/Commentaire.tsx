e0e75db65e51eabd3bbe56862ece9adad37c8b29
unknown <arnaud.defrancq@laposte.net>
2024-10-18 17:31:59 +0200

    [+] COMMENTAIRE : Ajout du Style OK

--- FRONT/src/Components/mainPage/feeds/card/commentaire/Commentaire.tsx
+++ FRONT/src/Components/mainPage/feeds/card/commentaire/Commentaire.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import Style from './Commentaire.module.css'
 
 interface ICommentaireProps {
   id_post: number | undefined
@@ -7,7 +10,20 @@ interface ICommentaireProps {
 const Commentaire: React.FunctionComponent<ICommentaireProps> = () => {
   return (
     <>
-      <p>ICI les commentaires pour un post</p>
+       <form className={`${Style.formCom} form`}>
+          <div className={Style.contentDiv}>
+              <div className={`${Style.group} form-group`}>
+                  <textarea  
+                      className={`${Style.input}`} 
+                      required
+                      placeholder=" "
+                      id='commentaire'
+                  />
+                  <label className={''} htmlFor="commentaire">Votre commentaire </label>
+              </div>
+          </div>
+          <button className=''><FontAwesomeIcon className={Style.icon} icon={faPaperPlane}/></button>
+        </form>
     </>
   );
 };
