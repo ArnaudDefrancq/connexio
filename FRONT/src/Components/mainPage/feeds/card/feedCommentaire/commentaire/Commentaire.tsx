3e9ad3fb43bdfca197bf1ee173682e959bd8d483
unknown <arnaud.defrancq@laposte.net>
2024-10-28 09:20:58 +0100

    [+] COMMENTAIRE : Ajout du btn pour delete un com Style OK

--- FRONT/src/Components/mainPage/feeds/card/feedCommentaire/commentaire/Commentaire.tsx
+++ FRONT/src/Components/mainPage/feeds/card/feedCommentaire/commentaire/Commentaire.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useContext, useState } from 'react';
 import Style from "./Commentaire.module.css";
 import { CommentaireWithProfil } from '../../../../../../Types/Commentaire';
 import AuthorCom from './authorCom/AuthorCom';
 import ComLike from './comLike/ComLike';
+import { UserContext } from '../../../../../../Context/UserContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical, faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 interface ICommentaireProps {
     com: CommentaireWithProfil,
@@ -12,10 +15,28 @@ interface ICommentaireProps {
 
 const Commentaire: React.FunctionComponent<ICommentaireProps> = ({ com, id_post }) => {
   
+  const { role, id_user } = useContext(UserContext);
+  const [isClick, setIsClick] = useState<boolean>(false);
+
+  const handleClick = ():void => {
+    setIsClick(prevState => !prevState);
+  }
+
+  const handleClickDelete =  ():void => {}
+
 
   return (
     <>
-      <div className={''}>
+      <div className={Style.divCom}>
+        {
+          (role == 1 || com.id_profil == id_user) && <button className={Style.button} onClick={handleClick}><FontAwesomeIcon className={Style.icon} icon={faEllipsisVertical}/></button>
+        }
+        {
+          (isClick) && 
+          <div className={Style.divTools}>
+            <button onClick={handleClickDelete}><FontAwesomeIcon className={Style.iconTools} icon={faTrash}/><span>Supprimer</span></button>
+          </div> 
+        }
         <div className={Style.authorCom}>
           <AuthorCom nom={com.nom} prenom={com.prenom} img={com.img_profil} id_profil={com.id_profil} createdAt={com.created_at}/>
         </div>
