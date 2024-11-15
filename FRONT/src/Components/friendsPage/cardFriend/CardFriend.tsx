e2cf7256e935669fa03648838b2da09e1f4038a1
unknown <arnaud.defrancq@laposte.net>
2024-11-15 16:36:36 +0100

    [+] AMI : Style CardAmi OK

--- FRONT/src/Components/friendsPage/cardFriend/CardFriend.tsx
+++ FRONT/src/Components/friendsPage/cardFriend/CardFriend.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Style from "./CardFriend.module.css";
 import { AmitieWithProfil } from '../../../Types/Amitie';
 
 interface ICardFriendProps {
@@ -6,10 +7,22 @@ interface ICardFriendProps {
 }
 
 const CardFriend: React.FunctionComponent<ICardFriendProps> = ({ profil }) => {
+
   return (
     <>
-        <div>
-            <p>test</p>
+        <div className={Style.cardFriend}>
+            <div className={Style.imgFriends}>
+              <img src={`${import.meta.env.VITE_URL_IMG}/imgProfil/${profil.id_profil_1}/profil/${profil.img_profil}`} alt="Photo de profil" className={Style.img}/>
+            </div>
+            <div  className={Style.infoContainer}>
+              <div>
+                <p>{profil.nom} {profil.prenom}</p>
+              </div>
+              <div className={Style.btnContainer}>
+                <button className={Style.accept}>Accepter</button>
+                <button className={Style.reject}>Refuser</button>
+              </div>
+            </div>
         </div>
     </>
   );
